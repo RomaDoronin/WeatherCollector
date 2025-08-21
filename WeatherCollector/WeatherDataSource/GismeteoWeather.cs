c201743b4d5907c9c802d2cbca9aef2bb6f4c7d0
Roman Doronin <roman.doronin.sklexp@yadnex.ru>
2025-08-21 11:45:55 +0300

    Фикс ветра от гисметео

--- WeatherCollector/WeatherDataSource/GismeteoWeather.cs
+++ WeatherCollector/WeatherDataSource/GismeteoWeather.cs
@@ -10,9 +10,9 @@ namespace WeatherCollector.WeatherDataSource
     {
         public List<string> StationList => new List<string>()
         {
-            "shakhunya-4322",
+            //"shakhunya-4322",
             "nizhny-novgorod-4355",
-            "vyksa-4375"
+            //"vyksa-4375"
         };
         public bool IsDivideDayNight => false;
 
@@ -61,28 +61,39 @@ namespace WeatherCollector.WeatherDataSource
 
         public void FindWindDirection(string source, WeekWeather currentWeekWeather)
         {
-            var commonKeyForParametr = "row-wind-direction";
-            var beginKeys = new List<string>() { "\"direction\">" };
+            var commonKeyForParametr = "widget-row-wind";
+            var beginKeys = new List<string>() { "/></svg></div>" };
             var endKey = '<';
             var dataAmount = WeatherProvider.numberForecastDaysMax + 1;
-            var precipitationParametrs = WeatherProvider.FindParametrs(source, commonKeyForParametr, beginKeys, endKey, dataAmount);
-            for (int count = 0; count < precipitationParametrs.Count; count++)
+            var windDirectionParametrs = WeatherProvider.FindParametrs(source, commonKeyForParametr, beginKeys, endKey, dataAmount);
+            for (int count = 0; count < windDirectionParametrs.Count; count++)
             {
-                currentWeekWeather.SetWindDirection(precipitationParametrs[count], count, WeekWeather.TimeOfDay.Night);
+                currentWeekWeather.SetWindDirection(windDirectionParametrs[count], count, WeekWeather.TimeOfDay.Night);
             }
         }
 
         public void FindWindSpeed(string source, WeekWeather currentWeekWeather)
         {
-            var commonKeyForParametr = "row-wind-speed";
+            var commonKeyForParametr = "widget-row-wind";
             var beginKeys = new List<string>() { "speed-value value=\"" };
             var endKey = '\"';
-            var dataAmount = WeatherProvider.numberForecastDaysMax + 1;
-            var precipitationParametrs = WeatherProvider.FindParametrs(source, commonKeyForParametr, beginKeys, endKey, dataAmount);
-            for (int count = 0; count < precipitationParametrs.Count; count++)
+            var dataAmount = (WeatherProvider.numberForecastDaysMax + 1) * 2;
+            var windSpeedParametrs = WeatherProvider.FindParametrs(source, commonKeyForParametr, beginKeys, endKey, dataAmount);
+
+            var minWindSpeed = -1;
+            for (int count = 0; count < windSpeedParametrs.Count; count++)
             {
-                var speed = precipitationParametrs[count].Replace(" ", "");
-                currentWeekWeather.SetWindSpeed(speed, count, WeekWeather.TimeOfDay.Night);
+                if (minWindSpeed == -1)
+                {
+                    minWindSpeed = int.Parse(windSpeedParametrs[count]);
+                }
+                else
+                {
+                    var maxWindSpeed = int.Parse(windSpeedParametrs[count]);
+                    var averageWindSpeed = (minWindSpeed + maxWindSpeed) / 2;
+                    currentWeekWeather.SetWindSpeed(averageWindSpeed.ToString(), count / 2, WeekWeather.TimeOfDay.Night);
+                    minWindSpeed = -1;
+                }
             }
         }
     }
